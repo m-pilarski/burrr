916d0d0fed33e0fb24b8bbe8f4d3e319cbfebe24
m-pilarski <pilarski@cryptolab.net>
2022-07-26 13:03:05 +0200

    add chunk_df

---
+++ R/chunk_df.R
@@ -0,0 +1,21 @@
+chunk_df <- function(.df, .n_rows=NULL, .n_chunks=NULL){
+
+  stopifnot(nrow(.df) > 0)
+  stopifnot(sum(sapply(list(.n_rows, .n_chunks), is.null)) == 1)
+  stopifnot(sum(sapply(list(.n_rows, .n_chunks), is.numeric)) == 1)
+
+  .df_row_seq <- seq_len(nrow(.df)) - 1
+
+  if(is.numeric(.n_rows)){
+    stopifnot(length(.n_rows) == 1 & .n_rows > 0 & .n_rows %% 1 == 0)
+    .df_split_id <- .df_row_seq %/% .n_rows + 1
+
+  }
+  if(is.numeric(.n_chunks)){
+    stopifnot(length(.n_chunks) == 1 & .n_chunks > 0 & .n_chunks %% 1 == 0)
+    .df_split_id <- ((.df_row_seq / nrow(.df)) * .n_chunks) %/% 1 + 1
+  }
+
+  split.data.frame(.df, .df_split_id)
+
+}
