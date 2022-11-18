0eacfc612d5093f821e1a47fd2a3fd50838bb48e
m-pilarski <pilarski@cryptolab.net>
2022-11-18 14:07:54 +0100

    added chunk function for vectors

--- R/chunk_df.R
+++ R/chunk_df.R
@@ -1,7 +1,6 @@
 chunk_df <- function(.df, .n_rows=NULL, .n_chunks=NULL){
 
   stopifnot(is.data.frame(.df))
-  stopifnot(nrow(.df) > 0)
   stopifnot(sum(sapply(list(.n_rows, .n_chunks), is.null)) == 1)
   stopifnot(sum(sapply(list(.n_rows, .n_chunks), is.numeric)) == 1)
 
@@ -17,6 +16,27 @@ chunk_df <- function(.df, .n_rows=NULL, .n_chunks=NULL){
     .df_split_id <- ((.df_row_seq / nrow(.df)) * .n_chunks) %/% 1 + 1
   }
 
-  split.data.frame(.df, .df_split_id)
+  unname(split.data.frame(.df, .df_split_id))
+
+}
+
+chunk_vector <- function(.vec, .n_elems=NULL, .n_chunks=NULL){
+
+  stopifnot(rlang::is_vector(.vec))
+  stopifnot(sum(sapply(list(.n_elems, .n_chunks), is.null)) == 1)
+  stopifnot(sum(sapply(list(.n_elems, .n_chunks), is.numeric)) == 1)
+
+  .vec_elems_seq <- seq_along(.vec) - 1
+
+  if(is.numeric(.n_elems)){
+    stopifnot(length(.n_elems) == 1 & .n_elems > 0 & .n_elems %% 1 == 0)
+    .vec_split_id <- .vec_elems_seq %/% .n_elems + 1
+  }
+  if(is.numeric(.n_chunks)){
+    stopifnot(length(.n_chunks) == 1 & .n_chunks > 0 & .n_chunks %% 1 == 0)
+    .vec_split_id <- ((.vec_elems_seq / length(.vec)) * .n_chunks) %/% 1 + 1
+  }
+
+  unname(split.default(.vec, .df_split_id))
 
 }
